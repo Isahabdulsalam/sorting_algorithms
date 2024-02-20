651c48c3ce30b393365e730e078edd8b45592207
Alphajay1 <ayakojamesty@gmail.com>
2024-02-19 21:29:24 -0800

    0-bubble_sort.c updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * swap_int - Swaps two integers
- * @a: Pointer to the first integer
- * @b: Pointer to the second integer
- */
-void swap_int(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
- * bubble_sort - Sorts an array of integers using Bubble Sort
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, unsorted_idx, sorted_idx;
+	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	unsorted_idx = size;
+	while (unsorted_idx > 0)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		sorted_idx = 0;
+		for (i = 0; i < unsorted_idx - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_int(&array[j], &array[j + 1]);
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				sorted_idx = i + 1;
 				print_array(array, size);
 			}
 		}
+		unsorted_idx = sorted_idx;
 	}
 }
